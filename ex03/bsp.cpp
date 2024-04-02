125a3ef70a9a46bfd6f9d802aecb1d68e9b7da98
Neko-Sato <hisatosi35350@gmail.com>
2024-04-03 03:51:51 +0900

    update

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 05:54:57 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/03 03:39:44 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/03 03:46:32 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	Point ap(point - a), bp(point - b), cp(point - c);
 	Fixed p[] = {ca * ap, ab * bp, bc * cp};
 
-	return (p[0] > 0 && p[1] > 0 && p[2] > 0) || (p[0] < 0 && p[1] < 0 && p[2] < 0);
+	return (p[0] >= 0 && p[1] >= 0 && p[2] >= 0) || (p[0] <= 0 && p[1] <= 0 && p[2] <= 0);
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 07:27:40 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/04/03 03:38:31 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/04/03 03:50:48 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 int main(void)
 {
+	std::cout << bsp(Point(-4.f, 6.f), Point(2.f, -8.f), Point(14.f, 16.f), Point(0.f, 8.222f)) << std::endl;
+	std::cout << bsp(Point(-4.f, 6.f), Point(2.f, -8.f), Point(14.f, 16.f), Point(0.f, 8.223f)) << std::endl;
 	std::cout << bsp(Point(0.f, -1.f), Point(1.f, 1.f), Point(-1.f, 1.f), Point(0.f, 0.f)) << std::endl;
 	std::cout << bsp(Point(0.f, -1.f), Point(1.f, 1.f), Point(-1.f, 1.f), Point(0.f, -1.f)) << std::endl;
 	std::cout << bsp(Point(0.f, -1.f), Point(1.f, 1.f), Point(-1.f, 1.f), Point(0.f, 1.f)) << std::endl;
